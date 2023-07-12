b153a2ccc85ea710d8b4c6a4f0e3b1baff3a38c9
DevJhonny99 <98360055+JhonnyDB99@users.noreply.github.com>
2023-07-11 23:40:27 -0300

    Update tabuada.js

--- tabuada.js
+++ tabuada.js
@@ -8,5 +8,4 @@ for (let fator = 1; fator <= 20; fator++) {
   resultado += " -> " + numero + " *  " + fator + " = " + numero * fator + "\n";
 }
 
-
 alert(" Resultado da tabuada de " + numero + ":\n\n" + resultado);
